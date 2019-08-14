91ed5def4f948b88e2daa2b31170cc53411fcc85
ucpr <hoge.uchihara@gmail.com>
2019-08-14 17:49:13 +0900

    feat: 問題文のところを1つにまとめた

--- postgres/init/problem.sql
+++ postgres/init/problem.sql
@@ -11,22 +11,5 @@ CREATE TABLE problems (
   memory_limit INTEGER NOT NULL,  -- MB
   score INTEGER NOT NULL,
   writer VARCHAR(16) NOT NULL
+  problem_detail TEXT
 );
-
-DROP TABLE IF EXISTS problem_details;
-CREATE TABLE problem_details (
-  problem_id VARCHAR(8) REFERENCES problems,
-  problem_text TEXT,
-  problem_constraint TEXT,
-  problem_input TEXT,
-  problem_output TEXT
-);
-CREATE INDEX ON problem_details (problem_id);
-
-DROP TABLE IF EXISTS problem_io_examples;
-CREATE TABLE problem_io_examples (
-  problem_id VARCHAR(8) NOT NULL REFERENCES problems,
-  input_example TEXT,
-  output_example TEXT
-);
-CREATE INDEX ON problem_io_examples (problem_id);
