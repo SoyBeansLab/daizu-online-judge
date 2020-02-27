afe463ac17449e6c703f9e77379ab49ea5b4d1db
ucpr <hoge.uchihara@gmail.com>
2020-02-28 02:40:26 +0900

    chore: add test data

---
+++ postgres/test/test_language.sql
@@ -0,0 +1,5 @@
+\c language;
+
+INSERT INTO languages VALUES ('C++', 'g++ 9.2.1', 'gnu_g++', 'g++ Main.cpp', './a.out');
+INSERT INTO languages VALUES ('C', 'gcc 9.2.1', 'gnu_gcc', 'gcc Main.c', './a.out');
+INSERT INTO languages VALUES ('Python3', 'Python 3.8.4', 'python3', 'python Main.py', '');
---
+++ postgres/test/test_problem.sql
@@ -0,0 +1,7 @@
+\c problem;
+
+INSERT INTO problems VALUES ('test0003', 'p10', '1', '初めてのWA', 2, 256, 100, 'ucpr', 'WAと出力しなさい');
+INSERT INTO problems VALUES ('test0003', 'p11', '2', '初めてのWA', 2, 256, 200, 'ucpr', 'WAと出力しなさい');
+INSERT INTO problems VALUES ('test0003', 'p12', '3', '初めてのWA', 2, 256, 300, 'ucpr', 'WAと出力しなさい');
+INSERT INTO problems VALUES ('test0003', 'p13', '4', '初めてのWA', 2, 256, 400, 'ucpr', 'WAと出力しなさい');
+INSERT INTO problems VALUES ('test0003', 'p14', '5', '初めてのWA', 2, 256, 500, 'ucpr', 'WAと出力しなさい');
