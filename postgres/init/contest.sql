53706d78d1d43c81b29b514d8a4f94df8780914a
ucpr <contact@ucpr.dev>
2020-05-30 01:28:14 +0900

    feat: tableの構成を変更

--- postgres/init/contest.sql
+++ postgres/init/contest.sql
@@ -4,13 +4,16 @@ CREATE DATABASE contest;
 
 DROP TABLE IF EXISTS contests;
 CREATE TABLE contests (
-  contest_id VARCHAR(8) UNIQUE NOT NULL,
+  contest_id VARCHAR(8) PRIMARY KEY,
   contest_name VARCHAR(32) NOT NULL,
   contest_start_date TIMESTAMP NOT NULL,
   contest_finish_date TIMESTAMP NOT NULL,
-  contest_time INTEGER NOT NULL,  -- minutes
+  contest_time INTEGER,  -- minutes
   writer VARCHAR(8) NOT NULL,
-  contest_description TEXT,
-  problem_number INTEGER DEFAULT 0 NOT NULL
+  description TEXT,
+  top_content TEXT,
+  problem_number INTEGER DEFAULT 0 NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-CREATE INDEX ON contests (contest_id);
+
