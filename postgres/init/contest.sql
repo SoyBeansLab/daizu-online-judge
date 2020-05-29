345c3ce6034a6805191a1350f8aa76e8d5efe0c6
ucpr <contact@ucpr.dev>
2020-05-30 01:28:31 +0900

    feat: update_atを更新するためのtriggerを実装

--- postgres/init/contest.sql
+++ postgres/init/contest.sql
@@ -17,3 +17,14 @@ CREATE TABLE contests (
   update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+-- update_atを更新するためのfunction
+-- http://to-c.hatenablog.com/entry/2017/04/17/225826
+CREATE FUNCTION set_update_timestamp() RETURNS OPAQUE AS '
+  BEGIN
+    new.update_at := ''now'';
+    RETURN new;
+  END;
+' LANGUAGE 'plpgsql';
+
+CREATE TRIGGER update_trigger BEFORE UPDATE ON contests FOR EACH ROW EXECUTE PROCEDURE set_update_timestamp();
+
