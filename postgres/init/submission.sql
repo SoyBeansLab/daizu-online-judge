f62083471124357d8f265b1c534d6dea045510c1
ucpr <contact@ucpr.dev>
2020-05-31 01:12:50 +0900

    feat: FKを追加した

--- postgres/init/submission.sql
+++ postgres/init/submission.sql
@@ -7,7 +7,7 @@ CREATE TABLE submittions (
   submit_date TIMESTAMP NOT NULL,
   username VARCHAR(16) NOT NULL,
   result VARCHAR(4) DEFAULT 'WJ',
-  language VARCHAR(8) NOT NULL,
+  language VARCHAR(8) REFERENCES languages(language),
   score INTEGER DEFAULT 0,
   test_case VARCHAR(8),
   source_code TEXT,
