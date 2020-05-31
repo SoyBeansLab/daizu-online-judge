73ce2f7e3b7db8285ca87cbb06df0cab2f74c224
ucpr <contact@ucpr.dev>
2020-05-31 16:30:12 +0900

    feat: registed_atからcreated_atに名前を変えた

--- postgres/init/registration.sql
+++ postgres/init/registration.sql
@@ -4,5 +4,5 @@ DROP TABLE IF EXISTS registrations;
 CREATE TABLE registrations (
   contest_id VARCHAR(8) REFERENCES contests(contest_id),
   username VARCHAR(16),
-  registed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
