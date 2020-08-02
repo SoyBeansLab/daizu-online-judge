bfa779f5a73c36a942e6e74ce317dea44c625b27
ucpr <contact@ucpr.dev>
2020-08-03 00:07:13 +0900

    feat: notificationsのDBを追加

---
+++ postgres/test/test_notification.sql
@@ -0,0 +1,26 @@
+\c doj;
+
+INSERT INTO notifications (id, description) VALUES (
+  'notificationId01',
+  'ver1をリリースしました'
+);
+INSERT INTO notifications (id, description) VALUES (
+  'notificationId02',
+  'ver2をリリースしました'
+);
+INSERT INTO notifications (id, description) VALUES (
+  'notificationId03',
+  'ver3をリリースしました'
+);
+INSERT INTO notifications (id, description) VALUES (
+  'notificationId04',
+  'ver4をリリースしました'
+);
+INSERT INTO notifications (id, description) VALUES (
+  'notificationId05',
+  'ver5をリリースしました'
+);
+INSERT INTO notifications (id, description) VALUES (
+  'notificationId06',
+  'ver6をリリースしました'
+);
